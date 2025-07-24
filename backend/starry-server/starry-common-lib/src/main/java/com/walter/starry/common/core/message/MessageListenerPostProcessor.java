79451c0874fa5ab203713bc8d6eecd901272e542
walter.tan <Walter.Tan@moco.com>
2025-07-24 16:13:59 +0800

    添加代码注释

--- backend/starry-server/starry-common-lib/src/main/java/com/walter/starry/common/core/message/MessageListenerPostProcessor.java
+++ backend/starry-server/starry-common-lib/src/main/java/com/walter/starry/common/core/message/MessageListenerPostProcessor.java
@@ -7,13 +7,13 @@ package com.walter.starry.common.core.message;
 public interface MessageListenerPostProcessor {
 
     /**
-     * 消费消息的前置处理
+     * 消费消息的前置处理。如果该方法在执行过程中出现异常，请在本方法内自行处理反向逻辑
      * @param objects
      */
     void preHandle(Object... objects);
 
     /**
-     * 消费消息的后置处理。如果前置处理方法{@link MessageListenerPostProcessor#preHandle(Object...)}抛出异常，则此方法将不会执行
+     * 消费消息的后置处理。如果前置处理方法{@link MessageListenerPostProcessor#preHandle(Object...)}抛出异常，此方法将不会执行
      * @param objects
      */
     void postHandle(Object... objects);
