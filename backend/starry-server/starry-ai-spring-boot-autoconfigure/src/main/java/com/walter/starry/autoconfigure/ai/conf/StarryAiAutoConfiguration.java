5d28561818ec2be3bc253c950fe587fe4029b99f
walter.tan <Walter.Tan@moco.com>
2025-09-13 21:46:54 +0800

    MCP客户端服务启动时，禁用预先执行初始化和获取工具列表

--- backend/starry-server/starry-ai-spring-boot-autoconfigure/src/main/java/com/walter/starry/autoconfigure/ai/conf/StarryAiAutoConfiguration.java
+++ backend/starry-server/starry-ai-spring-boot-autoconfigure/src/main/java/com/walter/starry/autoconfigure/ai/conf/StarryAiAutoConfiguration.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.mcp.client.autoconfigure.properties.McpClientCommonProperties;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.resolution.DelegatingToolCallbackResolver;
@@ -11,7 +12,6 @@ import org.springframework.ai.tool.resolution.SpringBeanToolCallbackResolver;
 import org.springframework.ai.tool.resolution.StaticToolCallbackResolver;
 import org.springframework.ai.tool.resolution.ToolCallbackResolver;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.GenericApplicationContext;
@@ -30,18 +30,20 @@ public class StarryAiAutoConfiguration {
     /**
      * 原生的ToolCallbackResolver存在问题：如果MCP服务不可用，MCP客户端启动时将会失败。因此重新定义此Bean来解决。参考：
      * {@link org.springframework.ai.model.tool.autoconfigure.ToolCallingAutoConfiguration#toolCallbackResolver(org.springframework.context.support.GenericApplicationContext, java.util.List, java.util.List)}
+     * @param mcpClientCommonProperties
      * @param applicationContext
      * @param toolCallbacks
      * @param tcbProviders
      * @return
      */
     @Bean
-    @ConditionalOnProperty(value = "spring.ai.mcp.client.initialized", havingValue = "false")
-    ToolCallbackResolver toolCallbackResolver(GenericApplicationContext applicationContext,
+    ToolCallbackResolver toolCallbackResolver(McpClientCommonProperties mcpClientCommonProperties, GenericApplicationContext applicationContext,
                                               List<ToolCallback> toolCallbacks, List<ToolCallbackProvider> tcbProviders) {
         List<ToolCallback> allFunctionAndToolCallbacks = new ArrayList<>(toolCallbacks);
         tcbProviders.stream().map(pr -> {
-            if(pr instanceof SyncMcpToolCallbackProvider || pr instanceof AsyncMcpToolCallbackProvider){
+            if(mcpClientCommonProperties.isInitialized()){
+                return List.of(pr.getToolCallbacks());
+            }else if(pr instanceof SyncMcpToolCallbackProvider || pr instanceof AsyncMcpToolCallbackProvider){
                 return new ArrayList<ToolCallback>();
             }else{
                 return List.of(pr.getToolCallbacks());
