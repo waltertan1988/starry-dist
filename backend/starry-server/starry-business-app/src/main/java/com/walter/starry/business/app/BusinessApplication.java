5ab6992a5735c6772d6b15f5692e7e8a4b612fac
waltertan <waltertan1988@163.com>
2026-02-28 17:38:20 +0800

    解决starry-business-app在集成spring-ai-starter-model-openai后引发spring-session失效的问题

--- backend/starry-server/starry-business-app/src/main/java/com/walter/starry/business/app/BusinessApplication.java
+++ backend/starry-server/starry-business-app/src/main/java/com/walter/starry/business/app/BusinessApplication.java
@@ -2,12 +2,14 @@ package com.walter.starry.business.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 
 /**
  * @author walter.tan
  */
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@SpringBootApplication
+@EnableRedisIndexedHttpSession(redisNamespace = "${app.security.namespace-prefix}:" + RedisIndexedSessionRepository.DEFAULT_NAMESPACE)
 public class BusinessApplication {
 
 	public static void main(String[] args) {
--- backend/starry-server/starry-business-app/src/main/java/com/walter/starry/business/app/controller/ai/ChatController.java
+++ backend/starry-server/starry-business-app/src/main/java/com/walter/starry/business/app/controller/ai/ChatController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.Random;
 import java.util.UUID;
 
 /**
